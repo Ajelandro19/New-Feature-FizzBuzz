7c2d86fd132d51c11fd08d6fad4b08bcaa0a4401
Ajelandro19 <almon190699@gmail.com>
2022-04-29 18:14:36 -0500

    Paso 6: Agregar test del controler y su solucion

--- test/controllers/Explorercontroller.test.js
+++ test/controllers/Explorercontroller.test.js
@@ -1,11 +1,22 @@
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
 describe("Tests para ExplorerController",()=>{
-    test("Requerimiento 1: Calcular todos los explorers en una misión", ()=>{
-        const explorersInNode=10;
-        expect(explorersInNode).toBe(10);
-    });
-    test("Requerimiento 2: Obtener el username del explorer en una misión",()=>{
-        const explorersInNode="ajolonauta1";
-        expect(explorersInNode).toBe("ajolonauta1");
+    test("Requerimiento: Obtener el trick de según un score dado", ()=>{
+        const score1 = 1;
+        const Fizz1=ExplorerController.getfuzzservice(score1);
+    
+        const score2 = 3;
+        const Fizz2= ExplorerController.getfuzzservice(score2); 
+    
+        const score3 = 5;
+        const Fizz3=ExplorerController.getfuzzservice(score3); 
+    
+        const score4 = 15;
+        const Fizz4=ExplorerController.getfuzzservice(score4);
+        
+        expect(Fizz1).toBe(1);
+        expect(Fizz2).toBe("FIZZ");
+        expect(Fizz3).toBe("BUZZ");
+        expect(Fizz4).toBe("FIZZBUZZ");
     });
 });
