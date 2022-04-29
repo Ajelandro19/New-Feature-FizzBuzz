2c3bfb1fccb881256c6fd5361ba23eeac3b4ff0c
Ajelandro19 <almon190699@gmail.com>
2022-04-29 17:50:01 -0500

    Paso 3: Crear codigo que haga pasar la prueba

--- test/services/FizzbuzzServices.test.js
+++ test/services/FizzbuzzServices.test.js
@@ -2,21 +2,21 @@ const FizzbuzzService = require("./../../lib/services/FizzbuzzService");
 
 describe("Tests para FizzBuzzService",()=>{
     test("Requerimiento: Obtener el trick de según un score dado", ()=>{
-        const score1 = {score: 1};
-        const Fizz1=FizzbuzzService.applyValidation(score1); // {name: "Explorer1", score: 1, trick: 1} 
+        const score1 = 1;
+        const Fizz1=FizzbuzzService.applyValidation(score1);
 
-        const score2 = {score: 3};
-        const Fizz2= FizzbuzzService.applyValidation(score2); // {name: "Explorer3", score: 3, trick: "FIZZ"}
+        const score2 = 3;
+        const Fizz2= FizzbuzzService.applyValidation(score2); 
 
-        const score3 = {score: 5};
-        const Fizz3=FizzbuzzService.applyValidation(score3); // {name: "Explorer5", score: 5, trick: "BUZZ"}
+        const score3 = 5;
+        const Fizz3=FizzbuzzService.applyValidation(score3); 
 
-        const score4 = {score: 15};
+        const score4 = 15;
         const Fizz4=FizzbuzzService.applyValidation(score4);
     
-        expect(Fizz1.trick).toBe(1);
-        expect(Fizz2.trick).toBe("FIZZ");
-        expect(Fizz3.trick).toBe("BUZZ");
-        expect(Fizz4.trick).toBe("FIZZBUZZ");
+        expect(Fizz1).toBe(1);
+        expect(Fizz2).toBe("FIZZ");
+        expect(Fizz3).toBe("BUZZ");
+        expect(Fizz4).toBe("FIZZBUZZ");
     });
 });
